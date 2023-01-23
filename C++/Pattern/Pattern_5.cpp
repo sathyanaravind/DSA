2662f9fd78b484203b30ba8fba7033e2dd335354
Aravind <77285092+sathyanaravind@users.noreply.github.com>
2023-01-23 23:59:26 +0530

    Add files via upload

---
+++ C++/Pattern/Pattern_5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=5;j<=1;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
