7edcb5dd328bf11ba42fa8d86c5fd13c46e8cbb6
Aravind <77285092+sathyanaravind@users.noreply.github.com>
2023-01-23 23:58:19 +0530

    Add files via upload

---
+++ C++/C++/7.1_ForLoop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i;
+    for(i=1; i<=10; i=i+1){
+        cout<<i<<endl;
+        cout<<"This is a for loop print for 10 times"<<endl;
+    }
+    cout<<i<<endl;
+    return 0;
+}
---
+++ C++/C++/8.1_whileLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i=1;
+    while(i<=5){
+        cout<<"while loop testing"<<endl;
+        i=i+1;
+    }
+    return 0;
+}
