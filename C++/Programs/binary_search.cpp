0edad5ac1fba43ab2024cc1c48c86ba337900e7c
Aravind <77285092+sathyanaravind@users.noreply.github.com>
2023-02-09 22:30:42 +0530

    Add files via upload

---
+++ C++/Programs/binary_search.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int binary_search(){
+
+}
+
+int main(){
+    int arr[5];
+    int i,n;
+    n = 5;
+    for(i=0;i<n;i++){
+            cin>>arr[i];
+    }
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
